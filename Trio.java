e437585ed805d6b4abe2b8e6bd9410c8d198bfbe
bNunley07 <benjamin.nunley@stu.daviess.kyschools.us>
2025-04-07 14:27:20 +0000

    iufdhg

--- Trio.java
+++ Trio.java
@@ -1,1 +1,25 @@
 //Write your answer here
+public class Trio implements MenuItem{
+    Sandwhich sandwhich;
+    Salad salad;
+    Drink drink;
+public Trio(int sandwhich, int salad, int drink)
+{
+sandwhich = sandwhich;
+salad = salad;
+drink = drink;
+}
+public String getName(){
+return "";
+}
+public  double getPrice(){
+return 0.0;
+}
+public ToString Trio(){
+
+return "" + sandwhich.getName() + "/" + salad.getName() + "/" + drink.getName() + ;
+
+}
+}
+
+
